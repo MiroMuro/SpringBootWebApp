efb266c41b29acb5b7a5f89be2a1b12077c655b4
MiroMuro <miro.laukkanen44@gmail.com>
2024-02-12 20:52:13 +0200

    Added cors handling for frontend

---
+++ src/main/java/com/Taskmanager/Config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.Taskmanager.Config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins("http://localhost:5173/") // Allows requests from any resource
+		.allowedMethods("GET","PUT","DELETE","POST")
+		.allowedHeaders("*");
+		
+	}
+	
+}
